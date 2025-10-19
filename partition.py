5c89ec216ff0d0752a01f9f0fefa254a45bc0b8a
Aryu <suwako_a-u-@hotmail.co.jp>
2025-10-19 22:28:48 +0900

    リファクタ

--- partition.py
+++ partition.py
@@ -1,25 +1,50 @@
-def partition(num, mem):
-    result = 0
-    for i in range(1, num + 1):
-        gokaku1, gokaku2 = [i * (3 * i - 1) // 2, i * (3 * i + 1) // 2]
-        if num < gokaku1 and num < gokaku2:
-            break
-        if num >= gokaku1:
-            result += mem[num - gokaku1] if i % 2 else -mem[num - gokaku1]
+class Partition:
+    def __init__(self, num = 4*(10**4), mod:int = 0):
+        """numまでの分割数をmodで割ったあまり（0の場合は剰余を考慮しない）で計算します"""
+        self.num = num
+        self.mod = mod
+        self.penta = self._calcPentagon(num)
+        self.data = [1]
+        for i in range(1, num + 1):
+            self.data.append(self._partition(i, self.data))
+    
+    def _calcPentagon(self, num: int) -> list:
+        """五角数と符号の事前計算を行います"""
+        result = []
+        for i in range(1, num + 1):
+            g1, g2 = i * (3 * i - 1) // 2, i * (3 * i + 1) // 2
+            if g1 > num and g2 > num:
+                break
+            sign = 1 if i % 2 else -1
+            if g1 <= num:
+                result.append((g1, sign))
+            if g2 <= num:
+                result.append((g2, sign))
+        result.sort()
+        return result
 
-        if num >= gokaku2:
-            result += mem[num - gokaku2] if i % 2 else -mem[num - gokaku2]
-    return result
+    def _partition(self, num: int, mem: list)-> int:
+        """DPによる分割数計算"""
+        result = 0
+        for g, sgn in self.penta:
+            if num < g:
+                break
+            result += sgn * mem[num - g]
+            if self.mod > 0:
+                result %= self.mod
+        return result
+
+    def getPartition(self, num: int) -> int:
+        """numに対しての分割数を取得します"""
+        assert self.num >= num
+        return self.data[num]
+
+    def getPartitionList(self) -> list:
+        """分割数をリストで返却します"""
+        return self.data
 
 
-def calcPartition(num):
-    result = [1]
-    for i in range(1, num + 1):
-        result.append(partition(i, result))
-    return result
-
-
-def inputNum(mes="InputNum > "):
+def inputNum(mes: str="InputNum > "):
     while True:
         try:
             print(mes, end="")
@@ -28,7 +53,7 @@ def inputNum(mes="InputNum > "):
             print('Input "Num" Please')
 
 
-def exportResult(result, filename):
+def exportResult(result: list, filename: str):
     with open(filename, "w", encoding="UTF-8") as f:
         for i in range(len(result)):
             mes = f"p({i}) = {result[i]}\n"
@@ -36,4 +61,5 @@ def exportResult(result, filename):
 
 
 num = inputNum()
-exportResult(calcPartition(num), "partition.txt")
+pt = Partition(num)
+exportResult(pt.getPartitionList(), "partition.txt")
